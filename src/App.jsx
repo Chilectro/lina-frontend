ad1661b3ed261149f0c8f1b65044787964381f40
Chilectro <rrojas122@gmail.com>
2026-09-04 13:23:28 -0400

    Update initial recaudado value in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 function App() {
   const metaTotal = 1785000;
   // 1. Convertimos el recaudado en un valor dinámico
-  const [recaudado, setRecaudado] = useState(1059000);
+  const [recaudado, setRecaudado] = useState(1190000);
   
   // Aseguramos que la barra no pase del 100% visualmente
   const porcentaje = Math.min((recaudado / metaTotal) * 100, 100);
